a80c4f4567d9f65dfc45717ee8e6bbdde3e00c44
lmonchon <lmonchon@LMONCHON>
2026-04-26 13:58:57 -0500

    Puerto para docker

--- AdminDashboard/Program.cs
+++ AdminDashboard/Program.cs
@@ -9,17 +9,22 @@ builder.Services.AddSession();
 builder.Services.AddHttpClient<AuthService>();
 builder.Services.AddHttpClient<AdminService>();
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(int.Parse(port));
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
